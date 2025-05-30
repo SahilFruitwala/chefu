a5efdbaa38157789a591a1bc8ed287911736a6bb
SahilFruitwala <sahilfruitwala007@gmail.com>
2025-05-30 18:42:15 -0500

    added theme

--- src/app/app/meal-plan-history/page.tsx
+++ src/app/app/meal-plan-history/page.tsx
@@ -35,11 +35,40 @@ export default function HistoryPage() {
         const mealPlan = await getMealPlanById(savedMealPlans[0].id, userId);
         setSelectedMealPlan(mealPlan[0]);
         setIsLoading(false);
+      } else {
+        setIsLoading(false);
       }
     };
 
     loadRecipes();
   }, []);
+
+  if (isLoading) {
+    return (
+      <div className="flex items-center justify-center min-h-screen">
+        <svg
+          className="animate-spin h-8 w-8 text-primary"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+        >
+          <circle
+            className="opacity-25"
+            cx="12"
+            cy="12"
+            r="10"
+            stroke="currentColor"
+            strokeWidth="4"
+          ></circle>
+          <path
+            className="opacity-75"
+            fill="currentColor"
+            d="M4 12a8 8 0 018-8v4a4 4 0 00-4 4H4z"
+          ></path>
+        </svg>
+      </div>
+    );
+  }
 
   const onRecipeSelect = async (recipeId: number) => {
     setIsLoading(true);
--- src/app/app/recipe-history/page.tsx
+++ src/app/app/recipe-history/page.tsx
@@ -34,11 +34,24 @@ export default function HistoryPage() {
         const recipe = await getRecipeById(savedRecipes[0].id, userId);
         setSelectedRecipe(recipe[0]);
         setIsLoading(false);
+      } else {
+        setIsLoading(false);
       }
     };
 
     loadRecipes();
   }, []);
+
+  if(isLoading) {
+    return (
+      <div className="flex items-center justify-center min-h-screen">
+        <svg className="animate-spin h-8 w-8 text-primary" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+          <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
+          <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8v4a4 4 0 00-4 4H4z"></path>
+        </svg>
+      </div>
+    );
+  }
 
   const onRecipeSelect = async (recipeId: number) => {
     setIsLoading(true);
